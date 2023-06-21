a16e43528762c19e3865e1b33b881e49f66e80b9
kl9y <126046208+kl9y@users.noreply.github.com>
2023-06-21 18:31:24 -0400

    updated server

--- server.js
+++ server.js
@@ -15,33 +15,36 @@ app.use(express.json());
     res.sendFile(path.join(__dirname, 'store/public/index.html'));
   });
 
-app.post("/checkout", async (req, res) =>{
-
-    const items = req.body.items;
-    let lineItems = [];
-    items.forEach((item) =>{
+  app.post("/checkout", async (req, res) => {
+    try {
+      const items = req.body.items;
+      let lineItems = [];
+      items.forEach((item) => {
         console.log(item.id);
         console.log(item.quantity);
-        lineItems.push(
-            
-
-            {
-                price: item.id,
-                quantity: item.quantity
-            }
-        )
-    })
-
-    const session = await stripe.checkout.sessions.create({
+        lineItems.push({
+          price: item.id,
+          quantity: item.quantity,
+        });
+      });
+  
+      const session = await stripe.checkout.sessions.create({
         line_items: lineItems,
         mode: 'payment',
         success_url: 'http://localhost:3000/success',
         cancel: 'http://localhost:3000/cancel',
         submit_type: 'pay',
-    });
-    res.send(JSON.stringify({
-        url: session.url
-    }));
-});
+      });
+  
+      res.send(JSON.stringify({
+        url: session.url,
+      }));
+    } catch (error) {
+      console.error(error);
+      res.status(500).send(JSON.stringify({
+        error: 'An error occurred while processing the payment.',
+      }));
+    }
+  });
 
 app.listen(4000, ()=> console.log("listening"));
