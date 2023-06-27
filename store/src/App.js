6256595cf7b40ac0e17542e7736aa814515d5e8f
kl9y <126046208+kl9y@users.noreply.github.com>
2023-06-27 01:34:36 -0400

    removed un needed files

--- store/src/App.js
+++ store/src/App.js
@@ -1,18 +1,12 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route} from "react-router-dom";
-import Store from './Store';
+
 
 
 function App() {
   return (
-        <BrowserRouter>
-          <Routes>
-            <Route index element={<Store />} />
-            <Route path="success" element={<Store />} />
-            <Route path="cancel" element={<Store />} />
-          </Routes>
-        </BrowserRouter>
+        <div>KL9Y</div>
   );
 }
 
